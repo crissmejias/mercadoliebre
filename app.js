6a28dccc0b02a28b5311a386b62f59d73c65f70a
Cristhian Mejias Serrano <cristhianmejias495@gmail.com>
2022-04-06 11:38:06 -0500

    Cambios para heroku

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 app.use(express.static("public"));
 
-app.listen(process.env.port || 3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Servidor funcionando");
 });
 
